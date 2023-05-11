8c76f6d3b25a6f51928f3f8156b205176f3bc92f
SagarCoder007 <sagarvarma10001@gmail.com>
2023-05-12 00:07:24 +0530

    follow up

---
+++ calculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char opp;
+	int num1,num2;
+	
+	cout <<"choose your choice ('+','-','*','/'):-";
+	cin >>opp;
+	
+	cout<<"Enter The first Number: ";
+	cin>>num1;
+	
+	cout<<"Enter The second Number: ";
+	cin>>num2;
+	
+	switch(opp){
+		case '+':
+		cout<<num1 + num2;
+		break;
+		
+		case '-':
+		cout<<num1 - num2;
+		break;
+		
+		case '*':
+		cout<<num1 * num2;
+		break;
+		
+			case '/':
+			if(num2 != 0)
+			cout<<num1 / num2;
+			break;
+			
+	
+		return 0;
+	}
+}
